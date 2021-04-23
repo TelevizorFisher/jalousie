ee1011e13cbe9d340e013fc67f0bb4595f047a11
TelevizorFisher <>
2021-04-23 21:56:39 +0300

    Updates

---
+++ precache-manifest.9052eac2eca10284e9948300cd3d3e6b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a3d54f45d1a20f4093c77572e9d5a9",
+    "url": "/TelevizorFisher/jalousie/index.html"
+  },
+  {
+    "revision": "68d65eba379532366f2d",
+    "url": "/TelevizorFisher/jalousie/static/css/main.75aedb94.chunk.css"
+  },
+  {
+    "revision": "9f276623a5ee87bceccc",
+    "url": "/TelevizorFisher/jalousie/static/js/2.9bab29b9.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/TelevizorFisher/jalousie/static/js/2.9bab29b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d65eba379532366f2d",
+    "url": "/TelevizorFisher/jalousie/static/js/main.0fdcb785.chunk.js"
+  },
+  {
+    "revision": "bbd17ca28d9a1643ae45",
+    "url": "/TelevizorFisher/jalousie/static/js/runtime-main.80c37c47.js"
+  },
+  {
+    "revision": "69fe1804e1ff8b2b8746d1c1a6770dbb",
+    "url": "/TelevizorFisher/jalousie/static/media/FonGreen.69fe1804.jpg"
+  },
+  {
+    "revision": "3b1fa58a500ab35cc592bf9d6d457e9c",
+    "url": "/TelevizorFisher/jalousie/static/media/access_time_24px.3b1fa58a.svg"
+  },
+  {
+    "revision": "a4a29fafcc93b5a68e533644e43523ef",
+    "url": "/TelevizorFisher/jalousie/static/media/logoSun.a4a29faf.png"
+  },
+  {
+    "revision": "444ff4f48ae0b4007f8874648f744357",
+    "url": "/TelevizorFisher/jalousie/static/media/phone_24px.444ff4f4.svg"
+  }
+]);
